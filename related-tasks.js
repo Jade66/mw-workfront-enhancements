00980754034b627a039bd6abb180f18dc76a4154
jade <jim.ade@mathworks.com>
2015-09-09 08:17:05 -0400

    Add multiple colors and clearing
    You can now highlight different families of tasks in different colors
    and clear the highlighting.

--- related-tasks.js
+++ related-tasks.js
@@ -1,26 +1,52 @@
 (function(){
-  
+  var RELATED_TASK_CLASS = 'jg-related-task';
+  var PARENT_TASK_CLASS = 'jg-parent-task';
+  var COLOR_INDEX_ATTRIBUTE = 'jg-color-index';
+
+  var availableColors = [0,1,2,3,4,5];
+  var usedColors = [];
+
   var flagRelatedStories = function(parentTaskId) {
-    var markedBoxNodes = document.querySelectorAll('div.jg-related-task');
-    [].forEach.call(markedBoxNodes, function(it){
-      it.classList.remove('jg-related-task');
-    });
-    
+
     var storyBoxNodes = document.querySelectorAll('div.story');
+    var settingColor = false;
+    var removingColor = false;
+    var colorIndex;
+
     [].forEach.call(storyBoxNodes, function(it){
-      var parentLink = it.querySelector('.jg-parent-task');
+      var parentLink = it.querySelector('.' + PARENT_TASK_CLASS);
       if(parentLink && parentLink.getAttribute('data-objid') === parentTaskId) {
-        it.classList.add('jg-related-task');
+        if (it.classList.contains(RELATED_TASK_CLASS)) {
+          if(!removingColor) {
+            removingColor = true;
+            colorIndex = it.getAttribute(COLOR_INDEX_ATTRIBUTE);
+            availableColors.unshift(colorIndex);
+          }
+          it.classList.remove(RELATED_TASK_CLASS);
+          it.classList.remove(RELATED_TASK_CLASS + '-' + colorIndex);
+          it.removeAttribute(COLOR_INDEX_ATTRIBUTE);
+        } else {
+          if(availableColors.length === 0) {
+            return;
+          }
+          if(!settingColor) {
+            settingColor = true;
+            colorIndex = availableColors.shift();
+          }
+          it.classList.add(RELATED_TASK_CLASS);
+          it.classList.add(RELATED_TASK_CLASS + '-' + colorIndex);
+          it.setAttribute(COLOR_INDEX_ATTRIBUTE, colorIndex);
+        }
       }
     });
   };
-  
+
   setTimeout(function(){
     var storyBoxNodes = document.querySelectorAll('div.story');
     [].forEach.call(storyBoxNodes, function(it){
       it.addEventListener('click', function(event) {
         if(event.altKey) {
-          var parentLink = it.querySelector('.jg-parent-task');
+          var parentLink = it.querySelector('.' + PARENT_TASK_CLASS);
           if(parentLink) {
             var parentTaskId = parentLink.getAttribute('data-objid');
             flagRelatedStories(parentTaskId);
